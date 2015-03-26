aa7e22dd301a70be68abf46ece2e70baad766d6e
Benjamin Molina <benmomo@users.noreply.github.com>
2015-03-26 12:04:20 +0100

    Update test.sh

--- docker/test.sh
+++ docker/test.sh
@@ -3,7 +3,9 @@
 cd /root/poi_fusion_engine2/services
 wget http://localhost:8080/fic2_fe_inquiry_valencia/citysdk/categories/search?list=poi -q -O down.json >/dev/null
 if diff ./down.json ./down.json >/dev/null ; then
+  echo "Test passed OK"
   echo 0
 else
+  echo "The test json file is not the same as expected"
   echo 1
 fi
